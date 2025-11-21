cc10acc5391609722466850e52760154d4d0f2fb
IA-LINK <jyc.kom@gmail.com>
2025-11-21 05:45:49 +0100

    update food page section

--- food_delivery/lib/home/main_food_page.dart
+++ food_delivery/lib/home/main_food_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/wedgats/big_text.dart';
+import 'package:food_delivery/wedgats/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -14,28 +16,38 @@ class _MainFoodPageState extends State<MainFoodPage> {
       body: Column(
         children: [
           Container(
-        child: Container(
-          margin: EdgeInsets.only(top:45, bottom:15),
-          padding: EdgeInsets.only(left:20, right:20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(children: [Text("Country"), Text("City")]),
-              Container(
-                width: 35,
-                height: 35,
-                child: Icon(Icons.search, color:Colors.white,),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue,
+            margin: const EdgeInsets.only(top: 45, bottom: 15),
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BigText(text: "Nigeria", color: Colors.black),
+                    SmallText(text: "Kaduna", color: Colors.grey),
+                    Text("City"),
+                  ],
                 ),
-              ),
-            ],
+
+                Container(
+                  width: 35,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue,
+                  ),
+                  child: const Icon(Icons.search, color: Colors.white),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
         ],
-      )
+      ),
     );
   }
+}
+
+class AppColors {
+  static Color mainColor = Colors.black;
 }
--- food_delivery/lib/wedgats/small_text.dart
+++ food_delivery/lib/wedgats/small_text.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 
-class BigText extends StatelessWidget {
+class SmallText extends StatelessWidget {
   final Color color;
   final String text;
   double size;
-  TextOverflow overflow;
-    BigText({Key? key, this.color = const Color(0xFF89dad0), 
+  double height;
+    SmallText({Key? key, this.color = const Color(0xFFccc7c5),
      required this.text,
-    this.size = 28,
-    this.overflow = TextOverflow.ellipsis
+    this.size = 12,
+    this.height = 1.2,
+   
     }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      maxLines: 1,
-      overflow: overflow,
+     
       style: TextStyle(
         fontFamily: 'Roboto',
         fontSize: size,
         color: color,
-        fontWeight: FontWeight.w400,
+        
       ),
     );
   }
