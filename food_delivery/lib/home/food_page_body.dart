cbbe4cd0d981b2f340457c84d988e0828438d737
IA-LINK <jyc.kom@gmail.com>
2025-11-24 03:22:05 +0100

    update decoration image

--- food_delivery/lib/home/food_page_body.dart
+++ food_delivery/lib/home/food_page_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/wedgats/big_text.dart';
+import 'package:food_delivery/wedgats/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -10,7 +12,8 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
         itemCount: 5,
@@ -22,17 +25,62 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-        image: DecorationImage(
-          image: AssetImage("assets/image/food2.png"),
-          fit: BoxFit.cover,
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+            image: DecorationImage(
+              image: AssetImage("assets/image/food2.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Nigeria Side"),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          10,
+                          (index) => Icon(
+                            Icons.star,
+                            color: Colors.orange,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      SmallText(text: "4.5"),
+                      SizedBox(width: 10),
+                      SmallText(text: "1287"),
+                      SizedBox(width: 10),
+                      SmallText(text: "comments"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
